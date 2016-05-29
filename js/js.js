360e48cebefd950601515024b29c98ba6ddcc29b
Hallurkn <hallurkn@hotmail.com>
2016-05-29 15:42:08 +0200

    Initial Commit - Canvas set up

---
+++ js/js.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Hallur on 29-05-2016.
+ */
+var stage, hero;
+var queue, prealoadText;
+
+function preload(){
+    stage = new createjs.Stage("canvas");
+    preloadText = new createjs.Text("0%", "30px monospace", "#FFF");
+    stage.addChild(preloadText);
+    queue = new createjs.LoadQueue(true);
+    queue.installPlugin(createjs.Sound);
+    queue.on('progress', loading);
+    queue.on('complete', startGame);
+
+    queue.loadManifest([
+    ])
+}
+
+function loading (e){
+    console.log(e.progress);
+    preloadText.text = Math.round(e.progress*100) + "%";
+    stage.update();
+}
